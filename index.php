6ad07decd5b8b879817e215ba9b7d9aa59ec6282
Utkarsh Choudhary <utkarsh121@gmail.com>
2024-02-15 22:36:11 -0500

    updates to index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
+    <title>My PHP App</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
-    <p>This is a simple PHP page.</p>
+    <h1>Welcome to My PHP App</h1>
+    <p>This is a simple PHP web application.</p>
 </body>
 </html>
