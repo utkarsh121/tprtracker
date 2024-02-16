9cdf09a4c57b26046a3ce6eea68fe2bc897b7f68
Utkarsh Choudhary <utkarsh121@gmail.com>
2024-02-15 22:57:07 -0500

    changed index.php

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My PHP App</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<div class="menu">
+    <ul>
+        <li><a href="clients/register.php">Register Client</a></li>
+    </ul>
+</div>
+
+<div class="content">
     <h1>Welcome to My PHP App</h1>
     <p>This is a simple PHP web application.</p>
+</div>
+
 </body>
 </html>
