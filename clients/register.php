57d0ba30c6dc9557e94c9aac78e41363b44eef5f
Utkarsh Choudhary <utkarsh121@gmail.com>
2024-02-15 23:01:52 -0500

    updated register.php

--- clients/register.php
+++ clients/register.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register New Client</title>
+    <title>Register Client</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+
+<div class="menu">
+    <ul>
+        <li><a href="register.php">Register Client</a></li>
+    </ul>
+</div>
+
+<div class="content">
     <h1>Register New Client</h1>
     <form action="process.php" method="POST">
         <label for="name">Name:</label>
@@ -16,5 +25,7 @@
         <input type="text" id="company" name="company" required><br><br>
         <button type="submit">Register</button>
     </form>
+</div>
+
 </body>
 </html>
